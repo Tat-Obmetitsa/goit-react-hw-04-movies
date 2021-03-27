6198401e261b06a2552e504679a8ccd8f044935c
Tat-Obmetitsa <twinkle.lif.love@gmail.com>
2021-03-27 19:03:57 +0200

    add Rating image rendering

--- src/Components/MoviesList/MoviesList.js
+++ src/Components/MoviesList/MoviesList.js
@@ -6,19 +6,22 @@ const MoviesList = ({ data }) => {
   return (
     <section>
       <h1 className={s.list__title}>Trending Movies</h1>
-      <ul>
+      <ul className={s.gallery}>
         {data.map(movie => (
-          <li key={movie.id}>
+          <li  className={s.gallery__card} key={movie.id}>
             <NavLink
               to={{
                 pathname: `/movies/${movie.id}`,
               }}
             >
+              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}  alt= ""
+            ></img>
               {movie.original_name ||
                 movie.name ||
                 movie.original_title ||
                 movie.title}
             </NavLink>
+
           </li>
         ))}
       </ul>
